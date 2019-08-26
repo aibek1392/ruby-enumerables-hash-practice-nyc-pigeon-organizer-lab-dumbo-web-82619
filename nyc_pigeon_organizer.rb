2eb41b57b89249f3775d5b447b116541cc84ab30
Aibek Ozhorov <ozhorov@gmail.com>
2019-08-26 14:31:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,11 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
+    
     value.each do |new_value, names|
       names.each do |name|
+        binding.pry
         if !new_hash[name]
           new_hash[name] = {}
         end 
